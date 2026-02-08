a1822a0d6bf624c0abb0ef7e442b863f79b6a80d
QUE54 <pornpawee9618@gmail.com>
2026-02-08 12:38:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.use(express.json())
 app.use(express.static("public"))
 
 mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => {
+    console.error("Mongo error:", err.message);
+    process.exit(1);
+  });
 
 const Spin = mongoose.model("Spin", {
   ip: String,
